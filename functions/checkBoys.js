c1f8bbb9f80890fde9efa7bd27ab13903f4fcd72
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-12-10 11:09:00 +0200

    very small fix:)

--- functions/checkBoys.js
+++ functions/checkBoys.js
@@ -3,28 +3,32 @@ const path = require("node:path");
 const { boysPath } = require("../pathes");
 
 async function checkBoys() {
-  const dataDir = await fs.readdir(boysPath);
-  const arrBoys = [];
+  try {
+    const dataDir = await fs.readdir(boysPath);
+    const arrBoys = [];
 
-  for (const string of dataDir) {
-    const pathToFile = path.join(boysPath, string);
+    for (const string of dataDir) {
+      const pathToFile = path.join(boysPath, string);
 
-    const dataFile = await fs.readFile(pathToFile, "utf-8");
+      const dataFile = await fs.readFile(pathToFile, "utf-8");
 
-    const parsedFile = JSON.parse(dataFile);
-    arrBoys.push(parsedFile);
-  }
-
-  arrBoys.forEach((item) => {
-    if (item.gender === "female") {
-      const GirlNeededToMove = path.join(`${item.name.toLowerCase()}.json`);
-
-      return fs.rename(
-        `./boys/${GirlNeededToMove}`,
-        `./girls/${GirlNeededToMove}`
-      );
+      const parsedFile = JSON.parse(dataFile);
+      arrBoys.push(parsedFile);
     }
-  });
+
+    arrBoys.forEach((item) => {
+      if (item.gender === "female") {
+        const GirlNeededToMove = path.join(`${item.name.toLowerCase()}.json`);
+
+        return fs.rename(
+          `./boys/${GirlNeededToMove}`,
+          `./girls/${GirlNeededToMove}`
+        );
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = checkBoys;
--- functions/checkGirls.js
+++ functions/checkGirls.js
@@ -3,28 +3,32 @@ const path = require("node:path");
 const { girlsPath } = require("../pathes");
 
 async function checkGirls() {
-  const dataDir = await fs.readdir(girlsPath);
-  const arrGirls = [];
+  try {
+    const dataDir = await fs.readdir(girlsPath);
+    const arrGirls = [];
 
-  for (const string of dataDir) {
-    const pathToFile = path.join(girlsPath, string);
+    for (const string of dataDir) {
+      const pathToFile = path.join(girlsPath, string);
 
-    const dataFile = await fs.readFile(pathToFile, "utf-8");
+      const dataFile = await fs.readFile(pathToFile, "utf-8");
 
-    const parsedFile = JSON.parse(dataFile);
-    arrGirls.push(parsedFile);
-  }
-
-  arrGirls.forEach((item) => {
-    if (item.gender === "male") {
-      const BoyNeededToMove = path.join(`${item.name.toLowerCase()}.json`);
-
-      return fs.rename(
-        `./girls/${BoyNeededToMove}`,
-        `./boys/${BoyNeededToMove}`
-      );
+      const parsedFile = JSON.parse(dataFile);
+      arrGirls.push(parsedFile);
     }
-  });
+
+    arrGirls.forEach((item) => {
+      if (item.gender === "male") {
+        const BoyNeededToMove = path.join(`${item.name.toLowerCase()}.json`);
+
+        return fs.rename(
+          `./girls/${BoyNeededToMove}`,
+          `./boys/${BoyNeededToMove}`
+        );
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = checkGirls;
