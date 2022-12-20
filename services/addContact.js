85783f068fcc4ee922dedcbc82cb5054cf6e013e
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-12-20 22:51:39 +0200

    add verify email to new contact

--- services/addContact.js
+++ services/addContact.js
@@ -11,6 +11,17 @@ const addContact = async ({ name, email, phone }) => {
     phone,
   };
 
+  const normalizedEmail = newContact.email.toLowerCase();
+
+  const findContactEmail = (newContactEmail) =>
+    contacts.find(({ email }) => email.toLowerCase() === newContactEmail);
+
+  if (findContactEmail(normalizedEmail)) {
+    throw new Error(
+      `Contact with email: ${email} already exist. Please enter another entities.`
+    );
+  }
+
   contacts.push(newContact);
 
   await updateContacts(contacts);
