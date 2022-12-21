98cbbf73695e88f78f67a6b44ef3b3868524d58d
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-12-21 19:36:00 +0200

    some fix

--- controllers/updateContact.js
+++ controllers/updateContact.js
@@ -17,7 +17,11 @@ const updateContact = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error.message);
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "Contact with req.id is not existed",
+    });
   }
 };
 
